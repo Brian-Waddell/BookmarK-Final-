24543b2952cdbcd10c0394a4edfb19e4a645f68f
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-11-15 15:57:57 +0000

    fixed user migration

--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_222922) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "\"username\"", name: "index_users_on_username", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
