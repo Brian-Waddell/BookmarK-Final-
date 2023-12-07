daf17d0f2e0e80e836d11106b5a518a07e2ec835
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-12-07 15:39:39 +0000

    Added seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,56 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # TODO: change to real email
-# User.create(email: "brianwddll2@gmail.com", username: "brian", password: "password")
+ #User.create(email: "brianwddll2@gmail.com", username: "brian", password: "password")
+
+ unless Rails.env.production?
+ 
+     names = ["brian", "alice", "bob", "calvin"]
+ 
+     names.each do |name|
+       u = User.create(
+         email: "#{name}@example.com",
+         username: name,
+         password: "password"
+       )
+       puts "added #{u.email}"
+     end
+     puts "done"
+   
+ 
+ 
+     100.times do |i| 
+ 
+       b = Book.create(
+         user_id: User.all.sample.id,
+         title: Faker::Book.title,
+         summary: Faker::TvShows::TheOffice.quote,
+         author: Faker::Book.author, 
+         genre: Faker::Book.genre,
+        
+        
+       )
+     end 
+
+       puts "adding comments"
+       250.times do |i|
+         commentable = nil
+         if i.odd?
+           commentable = Comment.all.sample
+         else
+           commentable = Book.all.sample
+         end
+ 
+         c = Comment.create(
+           user_id: User.all.sample.id,
+           body: Faker::Movies::HarryPotter.quote || Faker::TvShows::BigBangTheory.quote,
+           commentable_type: commentable.class.name, 
+           commentable_id: commentable.id
+         )
+       end
+ 
+       puts "done"
+     end
+   end 
+ end 
+ 
