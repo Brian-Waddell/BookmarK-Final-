2c067f5b3f917d1b3c1059becd827f552fd2d0ad
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-12-08 22:00:38 +0000

    working on creating chart in dashbord

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
     
     @q = Comment.ransack(params[:q])
     @results = authorize @q.result.includes(:user_id, :commentable_id)
+
+    @user = current_user
+    @commented_book_count = @user.comments.distinct.count(:user_id)
+
     
   end 
 end 
